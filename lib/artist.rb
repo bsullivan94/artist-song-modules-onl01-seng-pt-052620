6caebd328ae6f9138eced1714d583b09df4797e4
brody sullivan <brodysullivan0@gmail.com>
2020-06-30 03:13:20 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,20 +13,12 @@ class Artist
   end
 
  # def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  #  @@artists.detect{|a| a.name == name}
+#  end
 
   def self.all
     @@artists
   end
-
-  #def self.reset_all
-   # self.all.clear
-  #end
-
-  #def self.count
-   # self.all.count
-  #end
 
   def add_song(song)
     @songs << song
